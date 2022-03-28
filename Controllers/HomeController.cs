952c509d7a2ccc4e3a500079cad394f26e65751e
Ken Meech <kenmeech@gmail.com>
2022-03-27 21:40:12 -0500

    Get current user

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,31 +10,33 @@ using WETT.Data;
 
 namespace WETT.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly WETT_DBContext _dbContext;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly WETT_DBContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger, WETT_DBContext dBContext)
-        {
-            _logger = logger;
-            _dbContext = dBContext;
-        }
+		public HomeController(ILogger<HomeController> logger, WETT_DBContext dBContext)
+		{
+			_logger = logger;
+			_dbContext = dBContext;
+		}
 
-        public IActionResult Index(string userName)
-        {
-            return View( new HomeViewModel());
-        }
+		public IActionResult Index()
+		{
+			HomeViewModel model = new HomeViewModel();
+			model.UserId = User.Identity.Name;
+			return View(model);
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
--- Models/HomeViewModel.cs
+++ Models/HomeViewModel.cs
@@ -1,23 +1,30 @@
 ﻿namespace WETT.Models
 {
-    public class HomeViewModel
-    {
-        private string _userId;
-        public string userId {
-        get
-            {
-                if (_userId == null) {
-                    try
-                    {
-                        _userId = System.Security.Claims.ClaimsPrincipal.Current.Identity.Name;
-                    }
-                    catch
-                    {
-                        _userId = "unkown user";
-                    }
-                }
-                return _userId;
-            }
-        }
-    }
+	public class HomeViewModel
+	{
+		private string _userId;
+		public string UserId
+		{
+			get
+			{
+				if (_userId == null)
+				{
+					try
+					{
+						_userId = System.Security.Claims.ClaimsPrincipal.Current.Identity.Name;
+
+					}
+					catch
+					{
+						_userId = "unknown user";
+					}
+				}
+				return _userId;
+			}
+			set
+			{
+				_userId = value;
+			}
+		}
+	}
 }

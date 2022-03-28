e1b6545c62f5605ae1e92bb9e812fc6127445bf5
Ken Meech <kenmeech@gmail.com>
2022-03-27 21:17:55 -0500

    Show user id on home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WETT.Models;
+using WETT.Data;
 
 namespace WETT.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly WETT_DBContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WETT_DBContext dBContext)
         {
             _logger = logger;
+            _dbContext = dBContext;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string userName)
         {
-            return View();
+            return View( new HomeViewModel());
         }
 
         public IActionResult Privacy()
---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace WETT.Models
+{
+    public class HomeViewModel
+    {
+        private string _userId;
+        public string userId {
+        get
+            {
+                if (_userId == null) {
+                    try
+                    {
+                        _userId = System.Security.Claims.ClaimsPrincipal.Current.Identity.Name;
+                    }
+                    catch
+                    {
+                        _userId = "unkown user";
+                    }
+                }
+                return _userId;
+            }
+        }
+    }
+}

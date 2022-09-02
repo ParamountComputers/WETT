7a3460b4801da9735ec2f879222f2a33ad2b2840
Ken Meech <kenmeech@gmail.com>
2022-09-01 22:59:22 -0500

    Report Config 2

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -3,15 +3,15 @@ namespace WETT.Controllers
     using Microsoft.AspNetCore.Mvc;
     using System.Net;
     using System.Net.Mail;
-    using Telerik.Reporting.Services;
-    using Telerik.Reporting.Services.AspNetCore;
+    //using Telerik.Reporting.Services;
+    //using Telerik.Reporting.Services.AspNetCore;
 
-    [Route("api/reports")]
-    public class ReportsController : ReportsControllerBase
-    {
-        public ReportsController(IReportServiceConfiguration reportServiceConfiguration)
-            : base(reportServiceConfiguration)
-        {
-        }
-    }
+    //[Route("api/reports")]
+    //public class ReportsController : ReportsControllerBase
+    //{
+    //    public ReportsController(IReportServiceConfiguration reportServiceConfiguration)
+    //        : base(reportServiceConfiguration)
+    //    {
+    //    }
+    //}
 }
--- Startup.cs
+++ Startup.cs
@@ -18,9 +18,9 @@ using WETT.Services;
 using WETT.Infrastructure;
 using Constants = WETT.Infrastructure.Constants;
 using Microsoft.Identity.Web.UI;
-using Telerik.Reporting.Services;
+//using Telerik.Reporting.Services;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Telerik.Reporting.Cache.File;
+//using Telerik.Reporting.Cache.File;
 
 namespace WETT
 {
@@ -54,19 +54,19 @@ namespace WETT
 
 
 			//Telerik Reporting setup
-			services.AddControllers().AddNewtonsoftJson();
+			//services.AddControllers().AddNewtonsoftJson();
 			// Configure dependencies for ReportsController.
-			services.TryAddSingleton<IReportServiceConfiguration>(sp =>
-				new ReportServiceConfiguration
-				{
-					// The default ReportingEngineConfiguration will be initialized from appsettings.json or appsettings.{EnvironmentName}.json:
-					ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
-//					ReportingEngineConfiguration = ConfigurationHelper.ResolveConfiguration(sp.GetService<IWebHostEnvironment>()),
-					HostAppId = "ReportingCore3App",
-					Storage = new FileStorage(),
-					ReportSourceResolver = new UriReportSourceResolver(
-						System.IO.Path.Combine(sp.GetService<IWebHostEnvironment>().ContentRootPath, "Reports"))
-				});
+//			services.TryAddSingleton<IReportServiceConfiguration>(sp =>
+//				new ReportServiceConfiguration
+//				{
+//					// The default ReportingEngineConfiguration will be initialized from appsettings.json or appsettings.{EnvironmentName}.json:
+//					ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
+////					ReportingEngineConfiguration = ConfigurationHelper.ResolveConfiguration(sp.GetService<IWebHostEnvironment>()),
+//					HostAppId = "ReportingCore3App",
+//					Storage = new FileStorage(),
+//					ReportSourceResolver = new UriReportSourceResolver(
+//						System.IO.Path.Combine(sp.GetService<IWebHostEnvironment>().ContentRootPath, "Reports"))
+//				});
 
 
 			//			services.AddControllersWithViews(options =>

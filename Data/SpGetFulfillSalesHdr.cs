3c06dbc320b5280fa8a01f1cd8b04d0bc2404e3e
yoshinotodd <yoshino.todd@gmail.com>
2022-09-30 16:54:04 -0500

    fulfill sales add

---
+++ Data/SpGetFulfillSalesHdr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WETT.Data
+
+{
+    public class SpGetFulfillSalesHdr
+    {
+        public long CustomerOrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? DelveryDate { get; set; }
+        public string OrderNumber { get; set; }
+        public string Customer { get; set; }
+        public string City { get; set; }
+        public long CarrierID { get; set; }
+        public string CarrierDesc { get; set; }
+        public string Instructions { get; set; }
+        public string Status { get; set; }
+
+    }
+}
